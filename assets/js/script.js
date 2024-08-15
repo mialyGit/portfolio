d06390f05cb43cd02f896b37c719307f9818407b
Chikara <avotriniainamialison1106@gmail.com>
2024-08-15 09:57:13 +0300

    Change to dev back end

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["développeur fullstack", "web scrappeur"],
+    strings: ["développeur back-end", "développeur php", "développeur laravel", "développeur symfony", "web scrappeur"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
